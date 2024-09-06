80cd4b5a7ee1e8085e0e4219546a0d6f2aef25f5
Rajagopal37 <trajagopal37@gmail.com>
2024-09-06 12:32:40 +0530

    color

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,13 +5,13 @@ import AuthorManagement from "./AuthorManagement";
 const Dashboard = () => (
   <>
     <div className="row d-flex justify-content-center">
-      <h1 className="text-center border rounded border-dark w-75  p-3">
+      <h1 className=" text-center mt-2 border rounded border-dark bg-warning sticky  p-3">
         Library Management Dashboard
       </h1>
-      <div className="col-4">
+      <div className="col-4 border rounded border-dark p-2 m-2">
         <BookManagement />
       </div>
-      <div className="col-4">
+      <div className="col-4 border rounded border-dark  p-2 m-2 ">
         <AuthorManagement />
       </div>
     </div>
